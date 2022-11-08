6ecbba6140b567da554c405c2a1720f2f78660cc
Emanuel <emanuelpanait21@gmail.com>
2022-11-08 17:16:43 +0100

    Modified lab-intro-sql.sql

---
+++ lab-intro-sql.sql
@@ -0,0 +1,21 @@
+select * from actor;
+
+select title from film;
+
+select * from customer;
+
+select title from film;
+
+select name  as language from language;
+
+select count(store_id) as Number_of_Stores from store;
+
+select count(address) as Address_Count from adress;
+
+select count(first_name) as Employee_Count from staff;
+
+select first_name from staff;
+
+select last_name from staff;
+
+
